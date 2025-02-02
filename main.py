e8490a3f9859682898d1c8e0897449f843e0e7c7
Validan30 <validus17@gmail.com>
2025-02-02 13:33:06 -0800

    edited main.py with a function to print character_count

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def count_characters(char):
+    charl = char.lower()
+    char_count = {}
+    for words in charl:
+        if words in char_count:
+            char_count[words] += 1
+        else:
+            char_count[words] = 1
+    return char_count
+        
+    
 def count_words(text):
     words = text.split()
     return len(words)
@@ -6,9 +17,9 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         word_count = count_words(file_contents)
-        print(word_count)
+        character_count = count_characters(file_contents)
+        print(character_count)
 main()
-
 
 
 
