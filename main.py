9999471ee82d5ef13b469d933a9eec2f0feeccf9
Validan30 <validus17@gmail.com>
2025-02-02 14:48:06 -0800

    edited main.py to create a report of character count and put them in a neat order

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def sorting(dict):
+    return dict["count"]
+
+def character_list(text):
+    char_list = []
+    for characters in text:
+        if characters.isalpha() == True:
+            new_dict = {}
+            new_dict['char'] = characters
+            new_dict['count'] = text[characters]
+            char_list.append(new_dict)
+    char_list.sort(reverse=True, key=sorting)
+    return char_list
+
 def count_characters(char):
     charl = char.lower()
     char_count = {}
@@ -18,7 +32,14 @@ def main():
         file_contents = f.read()
         word_count = count_words(file_contents)
         character_count = count_characters(file_contents)
-        print(character_count)
+        char_list = character_list(character_count)
+        print("---Begin report of books/frankenstein.txt---")
+        print(f"{word_count} words found in the document")
+        for characters in char_list:
+            number = characters['count']
+            char = characters['char']
+            print(f"The '{char}' character was found {number} times")
+        print("--- End Report ---")
 main()
 
 
